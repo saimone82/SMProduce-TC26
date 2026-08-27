18cd78c7d7ad69ca69e9547af7004813847e582e
saimone82 <160996838+saimone82@users.noreply.github.com>
2026-08-26 19:57:35 -0700

    Finalize Pallets Shipping workflow: webapp/config/pallets_shipping_app.php

--- webapp/config/pallets_shipping_app.php
+++ webapp/config/pallets_shipping_app.php
@@ -2,5 +2,6 @@
 // Keep this value identical to BuildConfig.APP_TOKEN in the Android project.
 return [
     'token' => 'SMPS_2026_8e04f770b72d4ceeb9d9',
+    'skip_po_password_sha256' => '06727da8681e44e269eb26f378f188b9b88ec80912d6187a1aa92f3c38dd3a80',
     'enabled' => true,
 ];
