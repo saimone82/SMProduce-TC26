fd0e72a628fe95bcbbdd61a10b4214996d11ec4e
saimone82 <160996838+saimone82@users.noreply.github.com>
2026-09-25 18:45:26 -0700

    Add 2.0.59 PO overlay build scaffold

---
+++ overlay-2.0.59/stubs/src/main/java/com/smproduce/palletsshipping/Recovery52MultiActivity.java
@@ -0,0 +1,8 @@
+package com.smproduce.palletsshipping;
+
+import android.app.Activity;
+import org.json.JSONArray;
+
+public class Recovery52MultiActivity extends Activity {
+    void showOrders(JSONArray orders) {}
+}
